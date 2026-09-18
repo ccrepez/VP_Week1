17f9ed575f0f2c284d8cbc73c7c9dfed8ab7b924
Christa Caitlyn <140872769+ccrepez@users.noreply.github.com>
2026-09-18 08:15:49 +0700

    Update OrderRepository.kt

--- VP_Week1/lib/src/main/java/com/christa/lib/OrderRepository.kt
+++ VP_Week1/lib/src/main/java/com/christa/lib/OrderRepository.kt
@@ -1,4 +1,14 @@
 package com.christa.lib
 
-class OrderRepository {
+class OrderRepository : OrderInterface {
+    private val orders = ArrayList<Order>()
+
+    override fun viewOrders(): List<Order> {
+        return orders
+    }
+
+    override fun addOrder(order: Order): String {
+        orders.add(order)
+        return "Order for ${order.customerName} placed successfully!"
+    }
 }
